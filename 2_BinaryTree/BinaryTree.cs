be741fcf4becd6ce39a2fe3ba2f8c8353ed3eb55
NULL <nullnvrsk@gmail.com>
2020-08-25 23:38:12 +0300

    2 tasks for 6 lesson

---
+++ 2_BinaryTree/BinaryTree.cs
@@ -0,0 +1,80 @@
+﻿// [Скоморохов Максим]
+// Переписать программу, реализующее двоичное дерево поиска.
+// а) Добавить в него обход дерева различными способами;
+// б) Реализовать поиск в двоичном дереве поиска;
+
+using System;
+
+namespace _2_BinaryTree
+{
+    class Node
+    {
+        public int value;
+
+        public Node left;
+        public Node right;
+        public Node parent;
+    }
+
+    //---------------------------------------------------------------------
+    enum NodePosition { none, left, right };
+
+
+    //---------------------------------------------------------------------
+    class BinaryTree
+    {
+        public Node root;
+
+        //---------------------------------------------------------------------
+        public Node AddNode(Node parentNode, int value, NodePosition pos = NodePosition.none)
+        {
+            Node node = new Node();
+            node.parent = parentNode;
+            node.value = value;
+
+            if (pos == NodePosition.left)
+            {
+                node.parent.left = node;
+            }
+            else if(pos == NodePosition.right)
+            {
+                node.parent.right = node;
+            }
+
+            return node;
+        }
+
+        //---------------------------------------------------------------------
+        public void PreOrderTravers(Node root)
+        {
+            if (root != null)
+            {
+                Console.Write($"{root.value}, ");
+                PreOrderTravers(root.left);
+                PreOrderTravers(root.right);
+            }
+        }
+
+        //---------------------------------------------------------------------
+        public void SymetricOrderTravers(Node root)
+        {
+            if (root != null)
+            {
+                PreOrderTravers(root.left);
+                Console.Write($"{root.value}, ");
+                PreOrderTravers(root.right);
+            }
+        }
+
+        //---------------------------------------------------------------------
+        public void PostOrderTravers(Node root)
+        {
+            if (root != null)
+            {
+                PreOrderTravers(root.left);
+                PreOrderTravers(root.right);
+                Console.Write($"{root.value}, ");
+            }
+        }
+    }
+}
---
+++ 2_BinaryTree/Program.cs
@@ -0,0 +1,45 @@
+﻿// [Скоморохов Максим]
+// Переписать программу, реализующее двоичное дерево поиска.
+// а) Добавить в него обход дерева различными способами;
+// б) Реализовать поиск в двоичном дереве поиска;
+
+using System;
+using System.ComponentModel;
+
+namespace _2_BinaryTree
+{
+    //---------------------------------------------------------------------
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем бинарное дерево
+            BinaryTree tree = new BinaryTree();
+
+            Node n1 = tree.AddNode(null, 8);
+            tree.root = n1;
+            Node n2 = tree.AddNode(n1, 9, NodePosition.left);
+            Node n3 = tree.AddNode(n1, 15, NodePosition.right);
+            Node n4 = tree.AddNode(n2, 12, NodePosition.left);
+            Node n5 = tree.AddNode(n2, 6, NodePosition.right);
+            Node n6 = tree.AddNode(n3, 6, NodePosition.left);
+            Node n7 = tree.AddNode(n3, 12, NodePosition.right);
+            Node n8 = tree.AddNode(n4, 16, NodePosition.left);
+            Node n9 = tree.AddNode(n6, 5, NodePosition.left);
+            
+            Console.WriteLine("Обход дерева КЛП");
+            tree.PreOrderTravers(tree.root);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Обход дерева ЛКП");
+            tree.SymetricOrderTravers(tree.root);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Обход дерева ЛПК");
+            tree.PostOrderTravers(tree.root);
+
+            Console.ReadLine();
+        }
+    }
+}
